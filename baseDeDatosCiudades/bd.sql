140373280d22d33c3b6d31a7a1bd1024aa5b5088
SantiagoVasquezS <santiago.vasquez@mariaelvinia.edu.co>
2019-12-11 11:42:29 -0500

    Version 6.0
    Base de datos

---
+++ baseDeDatosCiudades/bd.sql
@@ -0,0 +1,15 @@
+create table habitante(
+    id int,
+    n_hijos int,
+    apellido varchar(200),
+    id_predioC int,
+    nitE varchar(20)
+);
+
+insert into habitante (id,n_hijos,apellido) values 
+(1,1, 'Villamil'),
+(2,1, 'Vasquez'),
+(3,2, 'Caro'),
+(4,3, 'Giraldo'),
+(5,4, 'Urrea');
+
