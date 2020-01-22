ce8f382547ac4cc6a1f5f615e55b6c1f56b3e36a
SantiagoVasquezS <santiago.vasquez@mariaelvinia.edu.co>
2020-01-22 11:44:28 -0500

    Version 7.0

--- baseDeDatosCiudades/bd.sql
+++ baseDeDatosCiudades/bd.sql
@@ -1,9 +1,47 @@
 create table habitante(
-    id int,
+    id varchar(15),
     n_hijos int,
     apellido varchar(200),
     id_predioC int,
-    nitE varchar(20)
+    nitE varchar(20),
+
+    primary key(id),
+    foreign key(id_predioC) references casa.id_predio,
+    foreign key(nitE) references empresa.nit
+);
+
+create table empresa(
+    nit varchar(20),
+    n_maquinas int,
+    f_fundacion date,
+
+    primary key(nit)
+);
+
+create table parque(
+    cod_placas varchar(8),
+    n_arboles int,
+    area float,
+
+    primary key(cod_placas)
+);
+
+create table casa(
+    id_predio varchar(20),
+    n_habitantes int,
+    estructura varchar(100),
+    idH varchar(15),
+
+    primary key(id_predio),
+    foreign key(idH) references habitante.id
+);
+
+create table asistencia(
+    idH varchar(15),
+    cod_placasParque varchar(8),
+    fecha_hora timestamp,
+
+    primary key
 );
 
 insert into habitante (id,n_hijos,apellido) values 
@@ -12,4 +50,6 @@ insert into habitante (id,n_hijos,apellido) values
 (3,2, 'Caro'),
 (4,3, 'Giraldo'),
 (5,4, 'Urrea');
+
+
 
